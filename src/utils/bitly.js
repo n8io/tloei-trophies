c954d6644439cfa22f604ddad33e3ed43199a11b
Nate Clark <n8@n8io.com>
2023-09-14 21:24:02 -0400

    ✨ Fix bitly url shortener

--- src/utils/bitly.js
+++ src/utils/bitly.js
@@ -1,38 +1,21 @@
+import { BitlyClient } from 'bitly';
 import { getConfig } from 'config';
-import BitlyClient from 'node-bitlyapi';
-import { path } from 'ramda';
 import { log } from './log';
 
-const {
-  BITLY_ACCESS_TOKEN,
-  BITLY_APP_CLIENT_ID,
-  BITLY_APP_CLIENT_SECRET,
-} = getConfig();
+const { BITLY_ACCESS_TOKEN } = getConfig();
 
-const bitly = new BitlyClient({
-  /* eslint-disable camelcase */
-  client_id: BITLY_APP_CLIENT_ID,
-  client_secret: BITLY_APP_CLIENT_SECRET,
-  /* eslint-enable camelcase */
-});
+const bitly = new BitlyClient(BITLY_ACCESS_TOKEN);
 
-bitly.setAccessToken(BITLY_ACCESS_TOKEN);
+const shorten = async longUrl => {
+  try {
+    const { link } = await bitly.shorten(longUrl);
 
-const shorten = longUrl =>
-  new Promise((resolve, reject) => {
-    bitly.shorten({ longUrl }, (err, raw) => {
-      if (err) {
-        log(`🛑 Failed to shorten url: ${longUrl}`);
+    return link;
+  } catch (err) {
+    log(`🛑 Failed to shorten url: ${longUrl}`);
 
-        return reject(err);
-      }
-
-      const url = path(['data', 'url'], JSON.parse(raw));
-
-      log(`👍 Url shortened to ${url}`);
-
-      return resolve(url);
-    });
-  });
+    throw err;
+  }
+};
 
 export { shorten };
